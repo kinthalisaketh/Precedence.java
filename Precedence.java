6268f1c675517fa16364a8d213d5afa3e32bbbc3
Kinthali Saketh <103805011+kinthalisaketh@users.noreply.github.com>
2023-09-28 18:29:35 +0530

    Create Precedence.java

---
+++ Precedence.java
@@ -0,0 +1,16 @@
+public class precedence {
+    
+    public static void main(String[] args){
+       int a= 6*5-34/2;
+        System.out.println(a);
+        //Precedence & Associativity
+        int b = 60/5-34*2;
+        System.out.println(b);
+        int f=1;
+        int g=4;
+        int h= 5;
+        int m= (b*b-(4*f*h))/(2*a);
+        System.out.println(m);
+
+    }
+}
